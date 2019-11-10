a3a27afd36a5a015b9fba5bce19ccdf151929d49
Justin Frahm <justinmfrahm@gmail.com>
2019-11-09 19:57:06 -0700

    First new module: vpc

---
+++ vpc/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+	cidr_block = "${var.vpc_cidr_block"}"
+	tags = merge({Name = "main-vpc"}, "${var.tags}")
+}
+
+resource "aws_subnet" "main" {
+	count = "${var.subnet_count}"
+	vpc_id = "${aws_vpc.main.id}"
+	cidr_block = cidrsubnet("${var.vpc_cidr_block}", "${var.subnets_size}", 
+	availability_zone = element(data.aws_availability_zones.available.names, count.index % length(data.aws_availability_zones.available.names))
+}
+
+data "aws_availability_zones" "available" {
+	state = "available"
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr_block" {
+	type = string
+	default = "10.0.0.0/8"
+}
+
+variable "subnet_count" {
+	type = number
+	default = 3
+}
+
+variable "subnet_size" {
+	type = number
+	default = 8
+	description = "Number of bits to add to the vpc cidr block mask"
+}

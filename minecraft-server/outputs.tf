7037433f8081aa8aa5c66227d1300a08c4e74ebd
Justin Frahm <justinmfrahm@gmail.com>
2020-05-09 12:35:15 -0600

    Add volume size output

--- minecraft-server/outputs.tf
+++ minecraft-server/outputs.tf
@@ -2,6 +2,10 @@ output "server_ip" {
 	value = aws_eip.server.public_ip
 }
 
+output "server_root_volume_size" {
+	value = aws_instance.server.root_block_device.volume_size
+}
+
 output "server_fqdn" {
 	value = aws_route53_record.server.fqdn
 }
